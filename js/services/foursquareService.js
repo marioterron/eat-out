3ec0930dd7f2802e02e0ea7da9fc27d1520724f1
Mario Terron <marioterron157@gmail.com>
2017-05-03 21:13:07 +0200

    modfied query placeholder and added radius in searchs

--- js/services/foursquareService.js
+++ js/services/foursquareService.js
@@ -2,7 +2,7 @@
 		.factory('foursquareService', function($http) {
 
 			function _getVenues(query, location) {
-				var url = 'https://api.foursquare.com/v2/venues/search?v=20170501&near=' + location + '&query=' + query + '&categoryId=4d4b7105d754a06374d81259&client_id=IXSMMTKC1C0EOWTOMSINDGHJLA12LXXLVIBH3J4DSKL5I3VW&client_secret=WK55K10UKPAJVGZKFI1XNCORVSKRYRLLWKFQ4TDO2U01AR2V'
+				var url = 'https://api.foursquare.com/v2/venues/search?v=20170501&radius=4500&near=' + location + '&query=' + query + '&categoryId=4d4b7105d754a06374d81259&client_id=IXSMMTKC1C0EOWTOMSINDGHJLA12LXXLVIBH3J4DSKL5I3VW&client_secret=WK55K10UKPAJVGZKFI1XNCORVSKRYRLLWKFQ4TDO2U01AR2V'
 				return $http.get(url)
 					.then(function(response) {
 						return response.data.response.venues
