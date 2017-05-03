81b2de137dddcb74582f8777155a3f9a25794010
Mario Terron <marioterron157@gmail.com>
2017-05-03 11:13:50 +0200

    modified url to API

--- js/services/foursquareService.js
+++ js/services/foursquareService.js
@@ -2,10 +2,10 @@
 		.factory('foursquareService', function($http) {
 
 			function _getVenues(query, location) {
-				var url = 'https://api.foursquare.com/v2/venues/suggestcompletion?v=20170501&near=' + location + '&query=' + query + '&categoryId=4d4b7105d754a06374d81259&client_id=IXSMMTKC1C0EOWTOMSINDGHJLA12LXXLVIBH3J4DSKL5I3VW&client_secret=WK55K10UKPAJVGZKFI1XNCORVSKRYRLLWKFQ4TDO2U01AR2V'
+				var url = 'https://api.foursquare.com/v2/venues/search?v=20170501&near=' + location + '&query=' + query + '&categoryId=4d4b7105d754a06374d81259&client_id=IXSMMTKC1C0EOWTOMSINDGHJLA12LXXLVIBH3J4DSKL5I3VW&client_secret=WK55K10UKPAJVGZKFI1XNCORVSKRYRLLWKFQ4TDO2U01AR2V'
 				return $http.get(url)
 					.then(function(response) {
-						return response.data.response.minivenues
+						return response.data.response.venues
 					})
 					// Filter if the venue not includes address
 					.then(function(venues) {
@@ -14,11 +14,11 @@
 						})
 					})
 					// Filter if the venue includes the word "baño"
-					// .then(function(venues) {
-					// 	return venues.filter(function(oVenue) {
-					// 		return oVenue.name.toLowerCase().includes('baño') != true;
-					// 	})
-					// })
+					.then(function(venues) {
+						return venues.filter(function(oVenue) {
+							return oVenue.name.toLowerCase().includes('baño') != true;
+						})
+					})
 			}
 
 			return {
